0cdb4914d357487271a8a131c45d19491a4fd98b
IT22280824 <it22280824@my.sliit.lk>
2025-05-09 15:43:34 +0530

    remove app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import './App.css'
+// import './App.css'
 import api from './api/axiosConfig.jsx';
 import Post from './components/post/post.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
